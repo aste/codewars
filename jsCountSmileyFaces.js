85a5cace5825828a023129866ef640e07298f635
Aste <aste@tuta.io>
2023-11-10 17:58:36 +0100

    Add a more concise jsCountSmileyFaces.js solution

--- jsCountSmileyFaces.js
+++ jsCountSmileyFaces.js
@@ -1,5 +1,1 @@
-function countSmileys(arr) {
-  const regexSmiley = /(:|;)(-|~)?(\)|D)/g;
-
-  return arr.filter((element) => element.match(regexSmiley)).length;
-}
+const countSmileys = (arr) => arr.filter((el) => el.match(/(:|;)(-|~)?(\)|D)/g)).length;
