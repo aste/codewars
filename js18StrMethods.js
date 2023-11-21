30e2c0f59c70934564c8068264e895113c3ab4e1
Aste <aste@tuta.io>
2023-11-21 15:10:56 +0100

    Add js18StrMethods.js 2nd iteration

--- js18StrMethods.js
+++ js18StrMethods.js
@@ -1,7 +1,6 @@
-function splitAndMerge(string, separator) {
-  let arrOfWords = string.split(" ");
-
-  arrOfWords.forEach((word, index, arr) => (arr[index] = word.split("").join(separator)));
-
-  return arrOfWords.join(" ");
+function splitAndMerge(str, sep) {
+  return str
+    .split(" ")
+    .map((word) => word.split("").join(sep))
+    .join(" ");
 }
