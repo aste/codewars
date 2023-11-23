2fe399893db83f5422aa8d8607228815638a29be
Aste <aste@tuta.io>
2023-11-23 18:28:21 +0100

    Add ternary operator for a more concise solution

--- js32MathMethods.js
+++ js32MathMethods.js
@@ -1,15 +1,4 @@
 function roundIt(n) {
-  const digits = String(n).split(".");
-
-  if (digits[0].length < digits[1].length) {
-    return Math.ceil(n);
-  } else if (digits[0].length > digits[1].length) {
-    return Math.floor(n);
-  } else {
-    return Math.round(n);
-  }
+  let [a, b] = n.toString().split(".");
+  return a.length > b.length ? Math.floor(n) : a.length === b.length ? Math.round(n) : Math.ceil(n);
 }
-
-console.log(roundIt(3.45), 4);
-console.log(roundIt(34.5), 34);
-console.log(roundIt(34.56), 35);
