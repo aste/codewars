4db2330694ebd2fd96b05130218e13bf6ca2bc93
Aste <aste@tuta.io>
2023-11-23 17:12:06 +0100

    Add a more concise solution with direct return of object literal

--- js27ArrayMethods.js
+++ js27ArrayMethods.js
@@ -1,19 +1,10 @@
 function countGrade(scores) {
-  let gradesGiven = {
-    S: 0,
-    A: 0,
-    B: 0,
-    C: 0,
-    D: 0,
-    X: 0,
+  return {
+    S: scores.filter((score) => score >= 100).length,
+    A: scores.filter((score) => score < 100 && score >= 90).length,
+    B: scores.filter((score) => score < 90 && score >= 80).length,
+    C: scores.filter((score) => score < 80 && score >= 60).length,
+    D: scores.filter((score) => score < 60 && score >= 0).length,
+    X: scores.filter((score) => score < 0).length,
   };
-
-  gradesGiven.S += scores.filter((score) => score >= 100).length;
-  gradesGiven.A += scores.filter((score) => score < 100 && score >= 90).length;
-  gradesGiven.B += scores.filter((score) => score < 90 && score >= 80).length;
-  gradesGiven.C += scores.filter((score) => score < 80 && score >= 60).length;
-  gradesGiven.D += scores.filter((score) => score < 60 && score >= 0).length;
-  gradesGiven.X += scores.filter((score) => score < 0).length;
-
-  return gradesGiven
 }
