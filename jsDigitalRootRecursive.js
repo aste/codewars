893494885b31b1597fc5061806bf789a1201d5db
Aste <aste@tuta.io>
2023-11-27 18:55:12 +0100

    Add a more concise version for jsDigitalRootRecursive.js

--- jsDigitalRootRecursive.js
+++ jsDigitalRootRecursive.js
@@ -1,4 +1,3 @@
 function digitalRoot(n) {
-  const accReducer = (accumulator, currentVal) => accumulator + parseInt(currentVal)
-  return n < 10 ? n : digitalRoot(n.toString().split("").reduce(accReducer, 0));
+  return n < 10 ? n : digitalRoot(n.toString().split("").reduce((acc, d) => acc + +d, 0))
 }
