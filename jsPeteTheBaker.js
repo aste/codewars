0103fd7d10f739e8d8eae42efd7e05f772944b79
Aste <aste@tuta.io>
2023-11-28 15:47:26 +0100

    Add initial solution for jsPeteTheBaker.js

---
+++ jsPeteTheBaker.js
@@ -0,0 +1,22 @@
+let recipe = { flour: 500, sugar: 200, eggs: 1 };
+let available = { flour: 1200, sugar: 1200, eggs: 5, milk: 200 };
+
+function cakes(recipe, available) {
+  let maxCakes = Infinity;
+
+  for (const ingredient in recipe) {
+    if (available[ingredient]) {
+      let cakeNum = Math.floor(available[ingredient] / recipe[ingredient]);
+      if (cakeNum < maxCakes) {
+        maxCakes = cakeNum;
+      }
+    } else {
+      maxCakes = 0;
+      break;
+    }
+  }
+
+  return maxCakes;
+}
+
+// console.log(cakes(recipe, available), 2);
