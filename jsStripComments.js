2cb908090fca8a94b88f80a54e19bff960712891
Aste <aste@tuta.io>
2023-11-29 14:18:02 +0100

    Add refactored and more concise solution for jsStripComments.js

--- jsStripComments.js
+++ jsStripComments.js
@@ -1,18 +1,6 @@
 function solution(input, markers) {
-  let arrOfInput = input.match(/\w+|[^\w]/g);
-  let stripArr = [];
-
-  for (let i = 0; i < arrOfInput.length; i++) {
-    if (markers.includes(arrOfInput[i])) {
-      if (stripArr[i - 1] === " ") {
-        stripArr.pop();
-      }
-      do {
-        i++;
-      } while (arrOfInput[i] !== "\n" && i < arrOfInput.length);
-    }
-    stripArr.push(arrOfInput[i]);
-  }
-
-  return stripArr.join("").trim();
+  return input
+    .split("\n")
+    .map((line) => markers.reduce((line, marker) => line.split(marker)[0].trim(), line))
+    .join("\n");
 }
