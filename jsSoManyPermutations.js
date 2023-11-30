19e47a14ca0e78a099d2cf80646894a2e0a8622f
Aste <aste@tuta.io>
2023-11-30 14:46:53 +0100

    Removed comments

--- jsSoManyPermutations.js
+++ jsSoManyPermutations.js
@@ -4,21 +4,12 @@ function permutations(string) {
 
   const uniquePermutations = new Set();
 
-  // Iterate over each character in the string
   for (let i = 0; i < string.length; i++) {
-    // Current character from which to create permutations
     const currentLetter = string[i];
-
-    // The remaining characters without the current character
     const remainingLetters = string.slice(0, i) + string.slice(i + 1);
-
-    // Call the permutations function recursively on the remaining characters
     const remainingLettersPermuted = permutations(remainingLetters);
 
-    // Iterate over all permutation returned from the recursive call
     for (let permutedString of remainingLettersPermuted) {
-      // Add the current char as the first character of each permutation, 
-      // then add to the permutation the Set to exclude non-unique values
       allPermutations.push(currentLetter + permutedString);
       uniquePermutations.add(currentLetter + permutedString);
     }
@@ -26,5 +17,3 @@ function permutations(string) {
 
   return Array.from(uniquePermutations);
 }
-
-console.log(permutations("abba"));
