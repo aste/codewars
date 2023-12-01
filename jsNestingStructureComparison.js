b54931f82a7ab5bc736c56fb5012c6a7de869de4
Aste <aste@tuta.io>
2023-12-01 14:53:28 +0100

    Add description for jsNestingStructureComparison.js

---
+++ jsNestingStructureComparison.js
@@ -0,0 +1,25 @@
+Array.prototype.sameStructureAs = function (other) {
+  // Return 'true' if and only if 'other' has the same
+  // nesting structure as 'this'.
+  // Note: You are given a function isArray(o) that returns
+  // whether its argument is an array.
+};
+
+// Complete the function/method (depending on the language) to return true/True when its argument is an array that has the same nesting structures and same corresponding length of nested arrays as the first array.
+
+// For example:
+
+//  // should return true
+// [ 1, 1, 1 ].sameStructureAs( [ 2, 2, 2 ] );
+// [ 1, [ 1, 1 ] ].sameStructureAs( [ 2, [ 2, 2 ] ] );
+
+//  // should return false
+// [ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2, 2 ], 2 ] );
+// [ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2 ], 2 ] );
+
+// // should return true
+// [ [ [ ], [ ] ] ].sameStructureAs( [ [ [ ], [ ] ] ] );
+
+// // should return false
+// [ [ [ ], [ ] ] ].sameStructureAs( [ [ 1, 1 ] ] );
+// For your convenience, there is already a function 'isArray(o)' declared and defined that returns true if its argument is an array, false otherwise.
