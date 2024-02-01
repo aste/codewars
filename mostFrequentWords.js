b324dce624f8013501b5d7b5b7bab363eb7ef19e
Aste <aste@tuta.io>
2024-02-01 15:49:22 +0100

    Add solution for mostFrequentWords.js

--- mostFrequentWords.js
+++ mostFrequentWords.js
@@ -1,11 +1,24 @@
-function topThreeMostFrequentWord(inputString) {
+function topThreeWords(text) {
+  const wordCountObj = {};
 
+  text
+    .toLowerCase()
+    .split(/[^\w']/g)
+    .filter(n => (n && n != "'"))
+    .forEach((word) => (wordCountObj[word] = (wordCountObj[word] || 0) + 1));
+
+  const top3 = Object.entries(wordCountObj)
+    .sort((a, b) => b[1] - a[1])
+    .map((arrElement) => arrElement[0])
+    .slice(0, 3);
+
+  return top3;
 }
 
 const testWord =
-  "In a village of La Mancha, the name of which I have no desire to call to mind, there lived not long since one of those gentlemen that keep a lance in the lance-rack, an old buckler, a lean hack, and a greyhound for coursing. An olla of rather more beef than mutton, a salad on most nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra on Sundays, made away with three-quarters of his income.";
+  "In a village of La Mancha, the name of which I have no desire to call to mind, there lived not long since one of those gentlemen that keep a lance in the lance-rack, an old buckler, a lean hack, and a greyhound for coursing. An olla of rather more beef than mutton, a salad on most nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra on Sundays, made away with three-quarters of his income. ' ' ' ' ' ' ' ' ' '";
 
-topThreeMostFrequentWord(testWord);
+topThreeWords(testWord);
 
 // DESCRIPTION:
 // Write a function that, given a string of text (possibly with punctuation and line-breaks), returns an array of the top-3 most occurring words, in descending order of the number of occurrences.
