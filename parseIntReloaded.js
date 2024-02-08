1d1779cfe23c45d4531cdfb599f27fdaaca1b390
Aste <aste@tuta.io>
2024-02-08 16:11:25 +0100

    Add description and test cases

--- parseIntReloaded.js
+++ parseIntReloaded.js
@@ -2,3 +2,34 @@ function parseInt(string) {
   // TODO: it's your task now
 }
 
+//   DESCRIPTION:
+//   In this kata we want to convert a string into an integer. The strings simply represent the numbers in words.
+
+//   Examples:
+
+//   "one" => 1
+//   "twenty" => 20
+//   "two hundred forty-six" => 246
+//   "seven hundred eighty-three thousand nine hundred and nineteen" => 783919
+//   Additional Notes:
+
+//   The minimum number is "zero" (inclusively)
+//   The maximum number, which must be supported is 1 million (inclusively)
+//   The "and" in e.g. "one hundred and twenty-four" is optional, in some cases it's present and in others it's not
+//   All tested numbers are valid, you don't need to validate them
+
+// million thousand hundred ()
+const Test = require("@codewars/test-compat");
+
+console.log(`${parseInt("one")}`);
+console.log("1");
+console.log("");
+console.log(`${parseInt("twenty")}`);
+console.log("20");
+console.log("");
+console.log(`${parseInt("two hundred forty-six")}`);
+console.log("246");
+console.log("");
+console.log(`${parseInt("seven hundred eighty-three thousand nine hundred and nineteen")}`);
+console.log("783919");
+console.log("");
