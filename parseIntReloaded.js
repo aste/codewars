b22b03c88ad34e13f2d5137c2ebbfb76fe97ada5
Aste <aste@tuta.io>
2024-02-09 17:05:19 +0100

    My solution with test cases

--- parseIntReloaded.js
+++ parseIntReloaded.js
@@ -1,26 +1,154 @@
-function parseInt(string) {
-  // TODO: it's your task now
-}
-
 //   DESCRIPTION:
 //   In this kata we want to convert a string into an integer. The strings simply represent the numbers in words.
 
-//   Examples:
+const numbers = {
+  zero: 0,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+  ten: 10,
+  eleven: 11,
+  twelve: 12,
+  thirteen: 13,
+  fourteen: 14,
+  fifteen: 15,
+  sixteen: 16,
+  seventeen: 17,
+  eighteen: 18,
+  nineteen: 19,
+  twenty: 20,
+  "twenty-one": 21,
+  "twenty-two": 22,
+  "twenty-three": 23,
+  "twenty-four": 24,
+  "twenty-five": 25,
+  "twenty-six": 26,
+  "twenty-seven": 27,
+  "twenty-eight": 28,
+  "twenty-nine": 29,
+  thirty: 30,
+  "thirty-one": 31,
+  "thirty-two": 32,
+  "thirty-three": 33,
+  "thirty-four": 34,
+  "thirty-five": 35,
+  "thirty-six": 36,
+  "thirty-seven": 37,
+  "thirty-eight": 38,
+  "thirty-nine": 39,
+  forty: 40,
+  "forty-one": 41,
+  "forty-two": 42,
+  "forty-three": 43,
+  "forty-four": 44,
+  "forty-five": 45,
+  "forty-six": 46,
+  "forty-seven": 47,
+  "forty-eight": 48,
+  "forty-nine": 49,
+  fifty: 50,
+  "fifty-one": 51,
+  "fifty-two": 52,
+  "fifty-three": 53,
+  "fifty-four": 54,
+  "fifty-five": 55,
+  "fifty-six": 56,
+  "fifty-seven": 57,
+  "fifty-eight": 58,
+  "fifty-nine": 59,
+  sixty: 60,
+  "sixty-one": 61,
+  "sixty-two": 62,
+  "sixty-three": 63,
+  "sixty-four": 64,
+  "sixty-five": 65,
+  "sixty-six": 66,
+  "sixty-seven": 67,
+  "sixty-eight": 68,
+  "sixty-nine": 69,
+  seventy: 70,
+  "seventy-one": 71,
+  "seventy-two": 72,
+  "seventy-three": 73,
+  "seventy-four": 74,
+  "seventy-five": 75,
+  "seventy-six": 76,
+  "seventy-seven": 77,
+  "seventy-eight": 78,
+  "seventy-nine": 79,
+  eighty: 80,
+  "eighty-one": 81,
+  "eighty-two": 82,
+  "eighty-three": 83,
+  "eighty-four": 84,
+  "eighty-five": 85,
+  "eighty-six": 86,
+  "eighty-seven": 87,
+  "eighty-eight": 88,
+  "eighty-nine": 89,
+  ninety: 90,
+  "ninety-one": 91,
+  "ninety-two": 92,
+  "ninety-three": 93,
+  "ninety-four": 94,
+  "ninety-five": 95,
+  "ninety-six": 96,
+  "ninety-seven": 97,
+  "ninety-eight": 98,
+  "ninety-nine": 99,
+  hundred: 100,
+  thousand: 1000,
+  million: 1000000,
+  and: null,
+};
 
-//   "one" => 1
-//   "twenty" => 20
-//   "two hundred forty-six" => 246
-//   "seven hundred eighty-three thousand nine hundred and nineteen" => 783919
+function parseInt(string) {
+  let strOfAllNumbers = string
+    .split(" ")
+    .map((word) => numbers[word])
+    .filter(Boolean);
+
+  let total = 0;
+  let lastVal = 0;
+  let tempCalVal = 0;
+
+  for (let i = 0; i < strOfAllNumbers.length; i++) {
+    let currentVal = strOfAllNumbers[i];
+    console.log(`currentVal ${currentVal}`);
+    if (lastVal > currentVal || tempCalVal === 0) {
+      tempCalVal = tempCalVal + currentVal;
+    } else {
+      tempCalVal = tempCalVal * currentVal;
+    }
+
+    if (currentVal === 1000000 || currentVal === 1000 || i === strOfAllNumbers.length - 1) {
+      total += tempCalVal;
+      tempCalVal = 0;
+    }
+
+    lastVal = currentVal;
+  }
+
+  return total;
+}
+
+let testStr = "three million eight hundred eighty-five thousand eight hundred and fourteen";
+
+parseInt(testStr);
+
 //   Additional Notes:
-
 //   The minimum number is "zero" (inclusively)
-//   The maximum number, which must be supported is 1 million (inclusively)
+//   The magnitudeimum number, which must be supported is 1 million (inclusively)
 //   The "and" in e.g. "one hundred and twenty-four" is optional, in some cases it's present and in others it's not
 //   All tested numbers are valid, you don't need to validate them
 
-// million thousand hundred ()
-const Test = require("@codewars/test-compat");
-
+// Test cases:
 console.log(`${parseInt("one")}`);
 console.log("1");
 console.log("");
@@ -32,4 +160,9 @@ console.log("246");
 console.log("");
 console.log(`${parseInt("seven hundred eighty-three thousand nine hundred and nineteen")}`);
 console.log("783919");
+console.log("");
+console.log(
+  `${parseInt("three million eight hundred eighty-five thousand eight hundred and fourteen")}`
+);
+console.log("3885814");
 console.log("");
