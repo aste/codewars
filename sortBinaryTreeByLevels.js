7a83b23e3fe4ef5e30476e278422673e30eee009
Aste <aste@tuta.io>
2024-02-21 17:26:59 +0100

    Refactor solution to a more concise version

--- sortBinaryTreeByLevels.js
+++ sortBinaryTreeByLevels.js
@@ -1,10 +1,8 @@
 function treeByLevels(rootNode) {
   const sortedByLevels = [];
-  const nodeQueue = [];
+  const nodeQueue = [rootNode];
 
-  if (typeof rootNode?.value !== "undefined") nodeQueue.push(rootNode);
-
-  while (nodeQueue.length > 0) {
+  while (nodeQueue.length) {
     if (typeof nodeQueue[0]?.value !== "undefined") sortedByLevels.push(nodeQueue[0].value);
     if (nodeQueue[0]?.left) nodeQueue.push(nodeQueue[0].left);
     if (nodeQueue[0]?.right) nodeQueue.push(nodeQueue[0].right);
