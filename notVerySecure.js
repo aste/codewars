7094a60e4331ee070b0ca5305d9e4daef1f191c0
Aste <aste@tuta.io>
2024-07-16 16:49:20 +0200

    Add refactored and more concise solution with regex, after reading other solutions

--- notVerySecure.js
+++ notVerySecure.js
@@ -1,21 +1,3 @@
-function alphanumeric(string) {
-    const inputToTest = string.toLowerCase().split('')
-    const legalCharacters = [
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-      ]
-    
-    let legality = true
-
-    if (inputToTest.length === 0) { legality = false }
-
-    for (let i = 0; i < inputToTest.length; i++) {
-        if (!legalCharacters.includes(inputToTest[i])){
-            legality = false
-            break
-        }
-    }
-
-    return legality
-}
+// Regex: The string must start '^' and end '$' with an alphanumeric character '[a-z\d]'
+// It must contain more than one alphanumeric characters '+' and the match is case-insensitive 'i'
+const alphanumeric = (string) => /^[a-z\d]+$/i.test(string);
