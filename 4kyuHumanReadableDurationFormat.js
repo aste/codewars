c52ecdd3b2a2cd16ee0473a5596d1a75fd46e6df
Aste <aste@tuta.io>
2024-07-19 13:08:24 +0200

    Add testcases

--- 4kyuHumanReadableDurationFormat.js
+++ 4kyuHumanReadableDurationFormat.js
@@ -1,7 +1,6 @@
-function formatDuration (seconds) {
-    // Complete this function
-  }
-  
+function formatDuration(seconds) {
+  // Complete this function
+}
 
 // DESCRIPTION:
 // Your task in order to complete this Kata is to write a function which formats a duration, given as a number of seconds, in a human-friendly way.
@@ -10,7 +9,7 @@ function formatDuration (seconds) {
 
 // It is much easier to understand with an example:
 
-// * For seconds = 62, your function should return 
+// * For seconds = 62, your function should return
 //     "1 minute and 2 seconds"
 // * For seconds = 3662, your function should return
 //     "1 hour, 1 minute and 2 seconds"
@@ -31,3 +30,8 @@ function formatDuration (seconds) {
 
 // A unit of time must be used "as much as possible". It means that the function should not return 61 seconds, but 1 minute and 1 second instead. Formally, the duration specified by of a component must not be greater than any valid more significant unit of time.
 
+console.log(formatDuration(1), "1 second");
+console.log(formatDuration(62), "1 minute and 2 seconds");
+console.log(formatDuration(120), "2 minutes");
+console.log(formatDuration(3600), "1 hour");
+console.log(formatDuration(3662), "1 hour, 1 minute and 2 seconds");
