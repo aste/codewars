b9dc84e2641c175a65a33e6141e92ef9fba4bef6
Aste <aste@tuta.io>
2024-07-19 12:26:39 +0200

    Add more concise solution for 5kyuSimplePigLatin.js

--- 5kyuSimplePigLatin.js
+++ 5kyuSimplePigLatin.js
@@ -1,16 +1,1 @@
-const pigIt = (str) => {
-  let pigStr = str
-    .split(" ")
-    .map((el) => {
-      if (/^[^.,!?;:'"(){}\[\]<>-]*$/.test(el)) {
-        return el.slice(1) + el.slice(0, 1) + "ay";
-      } else {
-        return el;
-      }
-    })
-    .join(" ");
-  return pigStr;
-};
-
-console.log(pigIt("Pig latin is cool"), "igPay atinlay siay oolcay !");
-console.log(pigIt("This is my string"), "hisTay siay ymay tringsay !");
+const pigIt = (str) => str.replace(/(\w)(\w*)(\s|$)/g, "$2$1ay$3");
