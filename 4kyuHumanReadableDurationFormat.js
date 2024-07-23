6a12b3999d6bfafdb9e7350ca78a2b1e0455613f
Aste <aste@tuta.io>
2024-07-23 18:15:21 +0200

    Add Human Readable Duration Format Function

--- 4kyuHumanReadableDurationFormat.js
+++ 4kyuHumanReadableDurationFormat.js
@@ -1,37 +1,46 @@
-function formatDuration(seconds) {
-  // Complete this function
+function formatDuration(inputSec) {
+  const durationsInSec = [31536000, 86400, 3600, 60, 1];
+  const durationNames = ["year", "day", "hour", "minute", "second"];
+  let remainingSec = inputSec;
+  let durationString = "";
+
+  for (let i = 0; i < durationsInSec.length; i++) {
+    if (durationsInSec[i] <= remainingSec) {
+      console.log(remainingSec);
+      let totalUnits = Math.floor(remainingSec / durationsInSec[i]);
+      let multiple = totalUnits > 1 ? "s" : "";
+      let postFix = "";
+      remainingSec = remainingSec - totalUnits * durationsInSec[i];
+      if (i === durationsInSec.length - 2 && remainingSec != 0) {
+        postFix = " and ";
+      } else if (i <= durationsInSec.length - 2 && remainingSec != 0) {
+        postFix = ", ";
+      }
+      durationString += `${totalUnits} ${durationNames[i]}${multiple}${postFix}`;
+      console.log(remainingSec);
+      console.log(durationString);
+      console.log("");
+    }
+  }
+
+  return durationString;
 }
 
-// DESCRIPTION:
-// Your task in order to complete this Kata is to write a function which formats a duration, given as a number of seconds, in a human-friendly way.
-
-// The function must accept a non-negative integer. If it is zero, it just returns "now". Otherwise, the duration is expressed as a combination of years, days, hours, minutes and seconds.
-
-// It is much easier to understand with an example:
-
-// * For seconds = 62, your function should return
-//     "1 minute and 2 seconds"
-// * For seconds = 3662, your function should return
-//     "1 hour, 1 minute and 2 seconds"
-// For the purpose of this Kata, a year is 365 days and a day is 24 hours.
-
-// Note that spaces are important.
-
-// Detailed rules
-// The resulting expression is made of components like 4 seconds, 1 year, etc. In general, a positive integer and one of the valid units of time, separated by a space. The unit of time is used in plural if the integer is greater than 1.
-
-// The components are separated by a comma and a space (", "). Except the last component, which is separated by " and ", just like it would be written in English.
-
-// A more significant units of time will occur before than a least significant one. Therefore, 1 second and 1 year is not correct, but 1 year and 1 second is.
-
-// Different components have different unit of times. So there is not repeated units like in 5 seconds and 1 second.
-
-// A component will not appear at all if its value happens to be zero. Hence, 1 minute and 0 seconds is not valid, but it should be just 1 minute.
-
-// A unit of time must be used "as much as possible". It means that the function should not return 61 seconds, but 1 minute and 1 second instead. Formally, the duration specified by of a component must not be greater than any valid more significant unit of time.
-
-console.log(formatDuration(1), "1 second");
-console.log(formatDuration(62), "1 minute and 2 seconds");
-console.log(formatDuration(120), "2 minutes");
-console.log(formatDuration(3600), "1 hour");
-console.log(formatDuration(3662), "1 hour, 1 minute and 2 seconds");
+console.log(formatDuration(1));
+console.log("1 second");
+console.log("");
+console.log(formatDuration(62));
+console.log("1 minute and 2 seconds");
+console.log("");
+console.log(formatDuration(120));
+console.log("2 minutes");
+console.log("");
+console.log(formatDuration(3600));
+console.log("1 hour");
+console.log("");
+console.log(formatDuration(3662));
+console.log("1 hour, 1 minute and 2 seconds");
+console.log("");
+console.log(formatDuration(5527800));
+console.log("63 days, 23 hours and 30 minutes");
+console.log("");
