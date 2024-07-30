fb059c50cd8ccd33c8d2855688bb46da042bee51
Aste <aste@tuta.io>
2024-07-30 12:34:00 +0200

    Add refactored solution with test cases

--- 3kyuMakeASpiral.js
+++ 3kyuMakeASpiral.js
@@ -6,11 +6,10 @@ function spiralize(n) {
     [0, -1],
     [-1, 0],
   ];
+  let timeToQuit = false;
   let curField = [0, 0];
   let curDir = 0;
   let nextDir = 1;
-  let straightSteps = 0;
-
   let count = 0;
 
   const checkField = ([dx, dy], factor, value) => {
@@ -21,13 +20,6 @@ function spiralize(n) {
     } else {
       return grid[x] === value;
     }
-    // console.log(`x: ${x}`);
-    // console.log(`y: ${y}`);
-    // if (grid[x] === undefined || grid[x][y] === undefined) {
-    //   return false;
-    // } else {
-    // }
-    // console.log(`grid[x][y]: ${grid[x][y]}`);
   };
 
   const moveCurField = ([dx, dy]) => {
@@ -37,67 +29,68 @@ function spiralize(n) {
 
   while (count <= n * n) {
     grid[curField[0]][curField[1]] = 1;
-    console.log(`---------------curField: ${curField}---------------`);
-    console.log(`curField[0]: ${curField[0]}`);
-    console.log(`curField[1]: ${curField[1]}`);
-    console.log(`directions[curDir]: ${directions[curDir]}`);
-    console.log(`checkField(directions[curDir], 2, 1): ${checkField(directions[curDir], 2, 1)}`);
-    console.log(
-      `checkField(directions[curDir], 1, undefined): ${checkField(
-        directions[curDir],
-        1,
-        undefined
-      )}`
-    );
-    console.log(`checkField(directions[nextDir], 3, 1): ${checkField(directions[nextDir], 3, 1)}`);
-    console.log(`checkField(directions[nextDir], 3, 1): ${checkField(directions[nextDir], 3, 1)}`);
+
+    if (timeToQuit) {
+      break;
+    }
 
     if (checkField(directions[curDir], 2, 1) || checkField(directions[curDir], 1, undefined)) {
-      console.log(`************* conditional 1 triggered ***************`);
-      console.log(`checkField(directions[curDir], 2, 1): ${checkField(directions[curDir], 2, 1)}`);
-      console.log(
-        `checkField(directions[curDir], 1, undefined): ${checkField(
-          directions[curDir],
-          1,
-          undefined
-        )}`
-      );
-      if (checkField(directions[nextDir], 3, 1) || checkField(directions[nextDir], 2, 1)) {
+      if (checkField(directions[nextDir], 2, 1)) {
         break;
       } else {
+        if (checkField(directions[nextDir], 3, 1)) { timeToQuit = true}
         curDir = (curDir + 1) % 4;
-
         nextDir = (nextDir + 1) % 4;
-        straightSteps = 0;
       }
     }
-    console.log(`[curDir]: ${[curDir]}`);
-    console.log(`directions[curDir]: ${directions[curDir]}`);
-
-    console.log(`curDir: ${curDir}`);
-    console.log(`nextDir: ${nextDir}`);
-    console.log(`curField: ${curField}`);
-    // console.log(`grid: ${grid[5][5]}`);
-    console.log(`directions[curDir]: ${directions[curDir]}`);
-    console.log(`directions[nextDir]: ${directions[nextDir]}`);
-
-    console.log(`grid:`);
-    console.log(grid);
-    console.log(`curField[0]: ${curField[0]}`);
-    console.log(`curField[1]: ${curField[1]}`);
 
     moveCurField(directions[curDir]);
     count++;
   }
 
-  console.log(grid);
   return grid;
 }
 
-console.log(spiralize(6));
-// console.log(00000
-//   ....0
-//   000.0
-//   0...0
-//   00000
-//   )
+let one = [
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [o, o, o, o, o, o, o, 1],
+  [1, 1, 1, 1, 1, 1, o, 1],
+  [1, o, o, o, o, 1, o, 1],
+  [1, o, o, o, o, 1, o, 1],
+  [1, o, 1, 1, 1, 1, o, 1],
+  [1, o, o, o, o, o, o, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1],
+];
+let two = [
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [o, o, o, o, o, o, o, 1],
+  [1, 1, 1, 1, 1, 1, o, 1],
+  [1, o, o, o, o, 1, o, 1],
+  [1, o, 1, o, o, 1, o, 1],
+  [1, o, 1, 1, 1, 1, o, 1],
+  [1, o, o, o, o, o, o, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+let test = [
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [o, o, o, o, o, o, o, 1],
+  [1, 1, 1, 1, 1, 1, o, 1],
+  [1, o, o, o, o, 1, o, 1],
+  [1, o, o, o, o, 1, o, 1],
+  [1, 1, 1, 1, 1, 1, o, 1],
+  [1, o, o, o, o, o, o, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+let result = [
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [o, o, o, o, o, o, o, 1],
+  [1, 1, 1, 1, 1, 1, o, 1],
+  [1, o, o, o, o, 1, o, 1],
+  [1, o, 1, o, o, 1, o, 1],
+  [1, o, 1, 1, 1, 1, o, 1],
+  [1, o, o, o, o, o, o, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1],
+];
+
