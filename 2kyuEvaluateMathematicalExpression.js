251e3aee4cebc28748252f7875a9ea8f8bc1a965
Aste <aste@tuta.io>
2024-08-01 13:41:02 +0200

    Add parenthesis handler functions

--- 2kyuEvaluateMathematicalExpression.js
+++ 2kyuEvaluateMathematicalExpression.js
@@ -1,9 +1,84 @@
 const calc = function (expression) {
   console.log(`expression:`);
   console.log(expression);
-  let arrOfParenthesis = expression.split(/(?=\()|(?<=\))/);
-  console.log(`arrOfParenthesis:`);
-  console.log(arrOfParenthesis);
+  let tokens = expression.split("");
+
+  if (tokens.includes(" ")) {
+    let plusMinusSignsDetected = 0;
+    let blankSpacesDetected = 0;
+    let blankSpacesCanBeDetected = false;
+
+    for (let i = 0; i < tokens.length; i++) {
+      if (tokens[i] === "-" || tokens[i] === "+") {
+        plusMinusSignsDetected += 1;
+        blankSpacesCanBeDetected = true;
+      }
+      if (tokens[i] === " " && blankSpacesCanBeDetected) {
+        blankSpacesDetected += 1;
+      }
+      if (!isNaN(tokens[i]) && tokens[i].trim() !== "") {
+        if (plusMinusSignsDetected >= 2 && blankSpacesDetected >= 2 && tokens[i - 1] === " ") {
+          return "Invalid";
+        }
+      }
+    }
+    tokens = tokens.filter((token) => token !== " ");
+  }
+
+  if (tokens.includes("(")) {
+    let startOfParenthesisIndex = -1;
+    let endOfParenthesisIndex;
+    let nestedLevels = 0;
+
+    for (let i = 0; i < tokens.length; i++) {
+      if (tokens[i] === "(") {
+        if (startOfParenthesisIndex === -1) {
+          startOfParenthesisIndex = i;
+        } else {
+          nestedLevels += 1;
+        }
+      }
+
+      if (tokens[i] === ")") {
+        if (nestedLevels === 0) {
+          endOfParenthesisIndex = i;
+          break;
+        } else {
+          nestedLevels -= 1;
+        }
+      }
+    }
+
+    if (startOfParenthesisIndex !== -1 && endOfParenthesisIndex !== undefined) {
+      const subExpression = tokens
+        .slice(startOfParenthesisIndex + 1, endOfParenthesisIndex)
+        .join("");
+
+      const subResult = calc(subExpression);
+
+      tokens.splice(
+        startOfParenthesisIndex,
+        endOfParenthesisIndex - startOfParenthesisIndex + 1,
+        subResult.toString()
+      );
+    }
+  }
+
+  if (tokens.includes("*") || tokens.includes("/")) {
+    let firstOperand;
+
+    for (let i = 0; i < tokens.length; i++) {}
+  }
+
+  multiply;
+
+  console.log(`tokens:`);
+  console.log(tokens.join(""));
+  console.log(`-(-1)): ${-(-1)}`);
+
+  //   let arrOfParenthesis = expression.split(/(?=\()|(?<=\))/);
+  //   console.log(`arrOfParenthesis:`);
+  //   console.log(arrOfParenthesis);
   //   let arrOfExpParts = expression.split(/([+\-()])/);
   //   console.log(`arrOfExpParts:`);
   //   console.log(arrOfExpParts);
@@ -15,35 +90,42 @@ const calc = function (expression) {
 
   //   }
   // evaluate `expression` and return result
+  return;
 };
 
-console.log(calc("1+1"));
-console.log(2);
-console.log("");
-console.log(calc("1 - 1"));
-console.log(0);
-console.log("");
-console.log(calc("1* 1"));
-console.log(1);
-console.log("");
-console.log(calc("1 /1"));
-console.log(1);
-console.log("");
-console.log(calc("-123"));
-console.log(-123);
-console.log("");
-console.log(calc("123"));
-console.log(123);
-console.log("");
-console.log(calc("2 /2+3 * 4.75- -6"));
-console.log(21.25);
-console.log("");
-console.log(calc("12* 123"));
-console.log(1476);
-console.log("");
-console.log(calc("2 / (2 + 3) * 4.33 - -6"));
-console.log(7.732);
+console.log(calc("2 / (2 + (3 * 4)) * 4.33 - -6"));
+console.log(calc("2 / (2 + (3 * 4)) * 4.33 - - 6"));
 
+// console.log(calc("1+1"));
+// console.log(2);
+// console.log("");
+// console.log(calc("1 - 1"));
+// console.log(0);
+// console.log("");
+// console.log(calc("1* 1"));
+// console.log(1);
+// console.log("");
+// console.log(calc("1 /1"));
+// console.log(1);
+// console.log("");
+// console.log(calc("-123"));
+// console.log(-123);
+// console.log("");
+// console.log(calc("123"));
+// console.log(123);
+// console.log("");
+// console.log(calc("2 /2+3 * 4.75- -6"));
+// console.log(21.25);
+// console.log("");
+// console.log(calc("12* 123"));
+// console.log(1476);
+// console.log("");
+// console.log(calc("2 / (2 + 3) * 4.33 - -6"));
+// console.log(7.732);
+// console.log("");
+
+console.log(calc("2 / (2 + (3 * 4)) * 4.33 - -6"));
+console.log(calc("2 / (2 + (3 * 4)) * 4.33 - - 6"));
 // You need to support multiple levels of nested parentheses, ex. (2 / (2 + 3.33) * 4) - -6
 
 // Whitespace
