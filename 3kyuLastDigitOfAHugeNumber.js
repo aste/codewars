3f561c37f09045e51ec34f641e030aae29de7af7
Aste <aste@tuta.io>
2024-08-28 16:35:32 +0200

    Add explanation for approach

--- 3kyuLastDigitOfAHugeNumber.js
+++ 3kyuLastDigitOfAHugeNumber.js
@@ -1,4 +1,49 @@
-function lastDigit(as) {}
+function lastDigit(exponentArray) {
+  console.log(exponentArray);
+  if (exponentArray.length < 2) return 1;
+  // All base numbers that ends with 0 to 9 have a repeatable last digit pattern result when
+  // raised to a given power/exponent, a pattern that repeats when the exponent is increased by 4
+  // Table of last digit pattern when raised a base number is raised to the power of
+  //     exponent: 1 | 2 3 4 | 5
+  // ------------------------------------
+  //  base number: 0 | 0 0 0 | 0  last digit when raised to exponent above
+  //               1 | 1 1 1 | 1
+  //               2 | 4 8 6 | 2
+  //               3 | 9 7 1 | 3
+  //               4 | 6 4 6 | 4
+  //               5 | 5 5 5 | 5
+  //               6 | 6 6 6 | 6
+  //               7 | 9 3 1 | 7
+  //               8 | 4 2 6 | 8
+  //               9 | 1 9 1 | 9
+
+  const minimalEffectiveExponent = (exp) => (exp % 4 === 0 ? (exp === 0 ? 0 : 4) : exp % 4);
+
+  let currentBase;
+  let currentExponent;
+
+  for (let i = exponentArray.length - 1; i >= 1; i--) {
+    currentExponent = currentExponent ? currentExponent : exponentArray[i];
+    currentBase = exponentArray[i - 1];
+
+    let currentEffectiveExponent = minimalEffectiveExponent(currentExponent);
+    let currentLastDigit = currentBase % 10;
+    
+
+    // console.log(`i: ${i}`);
+    // console.log(`currentBase: ${exponentArray[i - 1]}`);
+    // console.log(`currentLastDigit: ${currentLastDigit}`);
+    // console.log(`currentExponent: ${exponentArray[i]}`);
+    // console.log(`currentEffectiveExponent: ${currentEffectiveExponent}`);
+    // console.log(`currentLastDigit before: ${currentLastDigit}`);
+    
+    currentLastDigit = currentLastDigit ** currentEffectiveExponent % 10;
+    // console.log(`currentLastDigit after: ${currentLastDigit}`);
+    // console.log("");
+  }
+
+  return currentBase % 10;
+}
 
 // For a given list [x1, x2, x3, ..., xn] compute the last (decimal) digit of x1 ^ (x2 ^ (x3 ^ (... ^ xn))).
 
@@ -12,36 +57,47 @@ function lastDigit(as) {}
 
 console.log(lastDigit([]));
 console.log(1);
-console.log("");
+console.log("------------");
+
 console.log(lastDigit([0, 0]));
 console.log(1);
-console.log("");
+console.log("------------");
+
 console.log(lastDigit([0, 0, 0]));
 console.log(0);
-console.log("");
+console.log("------------");
+
 console.log(lastDigit([1, 2]));
 console.log(1);
-console.log("");
+console.log("------------");
+
 console.log(lastDigit([3, 4, 5]));
 console.log(1);
-console.log("");
+console.log("------------");
+
 console.log(lastDigit([4, 3, 6]));
 console.log(4);
-console.log("");
+console.log("------------");
+
 console.log(lastDigit([7, 6, 21]));
 console.log(1);
-console.log("");
+console.log("------------");
+
 console.log(lastDigit([12, 30, 21]));
 console.log(6);
-console.log("");
+console.log("------------");
+
 console.log(lastDigit([2, 2, 2, 0]));
 console.log(4);
-console.log("");
+console.log("------------");
+
 console.log(lastDigit([937640, 767456, 981242]));
 console.log(0);
-console.log("");
+console.log("------------");
+
 console.log(lastDigit([123232, 694022, 140249]));
 console.log(6);
-console.log("");
+console.log("------------");
+
 console.log(lastDigit([499942, 898102, 846073]));
 console.log(6);
